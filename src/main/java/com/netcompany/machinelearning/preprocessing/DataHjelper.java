7c0c399361011ee742261c76092e1bc83ed9804c
okrepp <okrepp91@gmail.com>
2017-02-12 18:42:28 +0100

    Refaktorerer litt så bilde-til-array kan gjenbrukes.

--- src/main/java/com/netcompany/machinelearning/preprocessing/DataHjelper.java
+++ src/main/java/com/netcompany/machinelearning/preprocessing/DataHjelper.java
@@ -1,14 +1,26 @@
 package com.netcompany.machinelearning.preprocessing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+
 public class DataHjelper {
+
+    public static int ANTALL_KLASSER = 10;
+    public static final int MNIST_HOYDE = 28;
+    public static final int MNIST_BREDDE = 28;
+
+    private static final Logger LOGG = LoggerFactory.getLogger(DataHjelper.class);
     private static final String ROTMAPPE = System.getProperty("user.dir");
     private static final String DATAMAPPE = ROTMAPPE + "/src/main/java/com/netcompany/machinelearning/data/";
     static String TRENINGSMAPPE = DATAMAPPE + "mnist_png/training";
     static String TESTMAPPE = DATAMAPPE + "mnist_png/testing";
-    public static int ANTALL_KLASSER = 10;
 
-    public static final int height = 28;
-    public static final int width = 28;
 
     public static void printBilde(final int[][] array) {
         final int w = array.length;
@@ -20,5 +32,32 @@ public class DataHjelper {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * @param bildeFil bildet som {@link File}
+     * @return en to-dimensjonal int array representasjon av bilde med verdier innenfor 0-255.
+     */
+    public static int[][] bildeTilIntArray(final File bildeFil) {
+        try {
+            final BufferedImage bilde = ImageIO.read(bildeFil);
+            final WritableRaster raster = bilde.getRaster();
+            final DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+            final int w = bilde.getWidth();
+            final int h = bilde.getHeight();
+
+            final int[][] array = new int[w][h];
+            for (int row = 0; row < w; row++) {
+                for (int col = 0; col < h; col++) {
+                    data.getElemFloat(row);
+                    array[row][col] = raster.getSample(row, col, 0);
+                }
+            }
+            return array;
+        } catch (final Exception e) {
+            LOGG.error("Bæsjen traff vifta ved lesing av bilde:" + bildeFil.getAbsolutePath());
+        }
+        return null;
     }
 }
--- src/main/java/com/netcompany/machinelearning/preprocessing/DataLaster.java
+++ src/main/java/com/netcompany/machinelearning/preprocessing/DataLaster.java
@@ -5,10 +5,6 @@ import com.twelvemonkeys.lang.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,18 +12,19 @@ import java.util.List;
 
 public final class DataLaster {
 
-    private final Logger LOGG = LoggerFactory.getLogger(this.getClass());
+    private final Logger LOGG = LoggerFactory.getLogger(DataLaster.class);
     private static final double ANDEL_AV_DATA = 0.05;
+    private static final double ALL_DATA = 1.0;
 
     private final Datasett treningsdata;
     private final Datasett testdata;
 
     public DataLaster(final Boolean lesInnHeleDatasettet) {
         if (lesInnHeleDatasettet) {
-            LOGG.info("Laster alle trenings- og testdata. Dette kan ta en stund...");
+            LOGG.info("Laster all trenings- og testdata. Dette kan ta en stund...");
 
-            treningsdata = lastBilder(1, DataHjelper.TRENINGSMAPPE);
-            testdata = lastBilder(1, DataHjelper.TESTMAPPE);
+            treningsdata = lastBilder(ALL_DATA, DataHjelper.TRENINGSMAPPE);
+            testdata = lastBilder(ALL_DATA, DataHjelper.TESTMAPPE);
 
             LOGG.info("Ferdig med å laste inn bilder.");
         } else {
@@ -50,48 +47,22 @@ public final class DataLaster {
         final ArrayList<String> dataklasseStier = Lists.newArrayList(f.list());
         for (final String mappe : dataklasseStier) {
             int antallPerKlasse = 0;
-            if (mappe.equalsIgnoreCase(".DS_STORE")) continue;
+            if (mappe.equalsIgnoreCase(".DS_STORE"))
+                continue;
             final String bildemappeSti = stiTilDatamappe + File.separator + mappe;
             final ArrayList<String> bildeStier = hentBildeStier(bildemappeSti);
             for (final String bildesti : bildeStier) {
                 if (antallPerKlasse++ > bildeStier.size() * andelAvData) {
                     break;
                 }
-                bilder.add(hentBildeSomIntArray(bildemappeSti + File.separator + bildesti));
+                bilder.add(DataHjelper.bildeTilIntArray(new File(bildemappeSti + File.separator + bildesti)));
                 fasit.add(Integer.parseInt(mappe));
             }
         }
-        final int[][][] bildeArray = new int[bilder.size()][DataHjelper.height][DataHjelper.width];
+        final int[][][] bildeArray = new int[bilder.size()][DataHjelper.MNIST_HOYDE][DataHjelper.MNIST_BREDDE];
         final int[] fasitArray = fasit.stream().mapToInt(Integer::intValue).toArray();
         bilder.toArray(bildeArray);
         return new Datasett(bildeArray, fasitArray);
-    }
-
-    /**
-     * @param bildeSti stien til ett bilde
-     * @return en to-dimensjonal int array representasjon av bilde med verdier innenfor 0-255.
-     */
-    private int[][] hentBildeSomIntArray(final String bildeSti) {
-        try {
-            final BufferedImage bilde = ImageIO.read(new File(bildeSti));
-            final WritableRaster raster = bilde.getRaster();
-            final DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-
-            final int w = bilde.getWidth();
-            final int h = bilde.getHeight();
-
-            final int[][] array = new int[w][h];
-            for (int row = 0; row < w; row++) {
-                for (int col = 0; col < h; col++) {
-                    data.getElemFloat(row);
-                    array[row][col] = raster.getSample(row, col, 0);
-                }
-            }
-            return array;
-        } catch (final Exception e) {
-            LOGG.info("Bæsjen traff vifta ved lesing av bilde:" + bildeSti);
-        }
-        return null;
     }
 
     private ArrayList<String> hentBildeStier(final String bildemappeSti) {
